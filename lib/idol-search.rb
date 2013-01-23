8df3c347ba511846cabe3668fe4ef873e74419ff
Shane Sherman <shane.sherman@gmail.com>
2013-01-22 21:30:01 -0600

    added abridged support

--- lib/idol-search.rb
+++ lib/idol-search.rb
@@ -41,15 +41,22 @@ module Idol
   module OptionalParams
     def has_optional_params(*params)
       params.flatten.each do |opt_param|
+
+        # reader
+        define_method("#{opt_param}") { @parameters[opt_param] }
+
+        # write the value to params
         define_method :"#{opt_param}" do |value|
           instance_variable_set("@#{opt_param}", value)
           @parameters[opt_param.gsub("_", "")] = value
           return self
         end
+
       end
     end
-
   end
+
+
 
   module BasicIdolFunctionality
     attr_accessor :url, :filters, :parameters, :raw_results
@@ -81,9 +88,11 @@ module Idol
       end
 
       request.http_post(*post_fields)
-      results = @raw_results ? body : Hash.from_xml(body)
-      if !@raw_results && results[:autnresponse][:responsedata][:abridged] 
-        # TODO: parse the abridged results
+      return body if @raw_results
+      results = if abridged
+        AbridgedResultsParser.new(body).parse
+      else
+        Hash.from_xml(body)
       end
       results
     end
@@ -95,11 +104,84 @@ module Idol
         post_fields << Curl::PostField.content("fieldtext", @filters.to_idol_syntax)
       end
 
-      puts "@params: #{@parameters.inspect}"
       @parameters.each do |name, values|
         post_fields << Curl::PostField.content(name, values)
       end
       post_fields
+    end
+  end
+
+  class AbridgedResultsParser
+    attr_accessor :result, :parsed_result
+    def initialize(result)
+      @result = result
+    end
+
+    def parse
+      fields = []
+      autoparsed_result = Hash.from_xml(@result)
+      xml = Nokogiri::XML(@result)
+
+      autn_ids = xml.xpath('//autn:abrid').first.content.split(",")
+      weights = xml.xpath('//autn:abrweight')[0].content.split(',').map{|w|w.to_i}
+
+      # parse out the field meta data
+      xml.xpath('//autn:abrfield').each do |field_data|
+        name = field_data.xpath('autn:abrname')[0].content
+        lengths = field_data.xpath('autn:abrlength')[0].content.split(',').map{|l| l.to_i }
+        values = field_data.xpath('autn:abrvalue')[0].content.strip
+        array_size_of_values = field_data.xpath('autn:abrnumber')[0].content.split(',').map{|n| n.to_i }
+        
+        current_values_position = 0
+        parsed_values = []
+
+        # the abridged results store everything in a big long string with the lengths to
+        # split on in a different comma seperated string. so it's parse out the values based
+        # on the value length
+        lengths.each do |length|
+          parsed_values << values[current_values_position, length]
+          current_values_position += length
+        end
+
+        # if the value is an array there's some additional magic that needs to be done to get the right array value
+        final_values = []
+        index = 0
+        array_size_of_values.each do |array_size|
+          if array_size == 1
+            final_values << parsed_values[index]
+          elsif array_size > 1
+            final_values << parsed_values[index, array_size]
+          end
+          index += 1
+        end
+
+        fields << {:name => name, :values => final_values} 
+      end
+
+      normalized_results = []
+      index = 0
+
+      # build a hash that mimics the hash from the unabridged normal results
+      autn_ids.each do |id|
+        result = {:id => id, :weight => weights[index], :content => {}}
+        fields.each do |field|
+          section, name = field[:name].split("/")
+          unless result[:content].has_key?(section)
+            result[:content][section] = {}
+          end
+          result[:content][section][name] = field[:values][index]
+        end
+        normalized_results << result
+        index += 1
+      end
+
+      response_data = autoparsed_result[:autnresponse][:responsedata]
+      response_data[:hit] = normalized_results
+      response_data.delete(:abrid)
+      response_data.delete(:abrweight)
+      response_data.delete(:abrfield)
+      @parsed_result = autoparsed_result
+      @parsed_result
     end
   end
 
--- test/test_idol-search.rb
+++ test/test_idol-search.rb
@@ -87,4 +87,46 @@ class TestIdolSearch < Test::Unit::TestCase
 
   end
 
+ABRIDGED_RESULTS_DATA = <<END_DATA
+<autnresponse xmlns:autn="http://schemas.autonomy.com/aci/">
+  <action>QUERY</action>
+  <response>SUCCESS</response>
+  <responsedata>
+    <autn:numhits>6</autn:numhits>
+    <autn:abridged>1</autn:abridged>
+    <autn:abrid>6747,6731,6702,6676,6515,6511</autn:abrid>
+    <autn:abrweight>9683,9683,9683,9683,9683,9683</autn:abrweight>
+    <autn:abrfield>
+    <autn:abrname>DOCUMENT/NGEN_TYPE</autn:abrname>
+    <autn:abrnumber>1,1,1,1,2,2</autn:abrnumber>
+    <autn:abrlength>8,8,8,8,7,7,7,7</autn:abrlength>
+    <autn:abrvalue>
+    BlogPostBlogPostBlogPostBlogPostArticleArticleArticleArticle
+    </autn:abrvalue>
+    </autn:abrfield>
+    <autn:abrfield>
+    <autn:abrname>DOCUMENT/ID</autn:abrname>
+    <autn:abrnumber>1,1,1,1,0,0</autn:abrnumber>
+    <autn:abrlength>24,24,24,24</autn:abrlength>
+    <autn:abrvalue>
+    50fdf2401772be26af00010f50fdc80f1772be269c00010a50fd82631772be269c00008d50fd82601772be269c000056
+    </autn:abrvalue>
+    </autn:abrfield>
+  </responsedata>
+</autnresponse>
+END_DATA
+
+  context "Abridged results parser" do
+    setup do
+      @parser = Idol::AbridgedResultsParser.new(ABRIDGED_RESULTS_DATA)
+      @parsed_result = @parser.parse
+    end
+
+    should "parse the data correctly" do
+      assert_equal 6, @parsed_result[:autnresponse][:responsedata][:hit].count
+    end
+
+
+  end
+
 end
